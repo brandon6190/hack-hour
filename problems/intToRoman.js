af3cd5f9904b4b355f99d5e3baa2d8bc398575d3
brandon6190 <brandonm6190@gmail.com>
2022-02-21 17:16:58 -0500

    Solution: -created a romans object in which each symbol is a property and its associated number is a value, and created an empty string to return at the end -looped through the romans object and while the input num is greater than the current roman numeral value, concat the current symbol to the string to be returned and substract the input num by the current roman numeral value. -return the string at the end of the process

--- problems/intToRoman.js
+++ problems/intToRoman.js
@@ -49,30 +49,28 @@ Constraints:
 
 
 function intToRoman(num) {
-//  const romans = {
-//    I: 1,
-//    V: 5,
-//    X: 10,
-//    L: 50,
-//    C: 100,
-//    D: 500,
-//    M: 1000
-//  };
-
   const romans = {
-    1: I,
-    5: V,
-    10: X,
-    50: L,
-    100: C,
-    500: D,
-    1000: M
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    V: 5,
+    IV: 4,
+    I: 1
   };
 
   let myRoman = '';
 
-  for () {
-    myRoman += romans[];
+  for (let numeral in romans) {
+    while (num >= romans[numeral]) {
+      myRoman += numeral;
+      num -= romans[numeral];
+    }
   }
 
   return myRoman;
