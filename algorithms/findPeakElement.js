ac205caad66d974a17730cfc4fb65e34a397a42a
brandon6190 <brandonm6190@gmail.com>
2022-03-05 21:32:02 -0500

    Solved findPeakElement at O(n) time

--- algorithms/findPeakElement.js
+++ algorithms/findPeakElement.js
@@ -31,7 +31,17 @@ Constraints:
 */
 
 function findPeakElement(nums) {
-  return nums;
+  if (nums.length === 1) return 0;
+
+  for (let i = 0; i < nums.length; i += 1) {
+    if (nums[i] > nums[i-1] && nums[i] > nums[i+1]) return i;
+  }
+
+  if (nums[0] > nums[1]) return 0;
+
+  if (nums[nums.length-1] > nums[nums.length-2]) return nums.length-1;
+
+  return 0;
 }
 
 
@@ -42,3 +52,6 @@ const nums2 = [1,2,1,3,5,6,4];
 
 console.log({nums1});
 console.log({nums2});
+
+console.log(findPeakElement(nums1));
+console.log(findPeakElement(nums2));
